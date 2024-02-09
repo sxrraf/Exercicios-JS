d69b26cf0b632e4e32906bae765a886f6966a8a1
yara <97001981+sxrraf@users.noreply.github.com>
2024-02-09 12:00:37 -0300

    Add files via upload

---
+++ exercicio4.js
@@ -0,0 +1,9 @@
+let valor = 9
+let sucessor = valor + 1
+let antecessor = valor - 1 
+console.log (valor )
+console.log ("valor:", valor)
+console.log (sucessor)
+console.log ("sucessor:", sucessor)
+console.log (antecessor)
+console.log ("antecessor:", antecessor)
